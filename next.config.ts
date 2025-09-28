89537cecafb4f615e191f98efea6556eea3b52b0
DS <ds@DSs-MacM1Pro.local>
2025-09-28 13:21:30 +0700

    update to base.dev commit

--- next.config.ts
+++ next.config.ts
@@ -5,15 +5,15 @@ const nextConfig: NextConfig = {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
   },
-  async redirects() {
-    return [
-      {
-        source: "/.well-known/farcaster.json",
-        destination: "https://api.farcaster.xyz/miniapps/hosted-manifest/01998e96-0135-13d3-da11-6601600a17b9",
-        permanent: false, // false = 307 Temporary Redirect
-      },
-    ];
-  }
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: "/.well-known/farcaster.json",
+  //       destination: "https://api.farcaster.xyz/miniapps/hosted-manifest/01998e96-0135-13d3-da11-6601600a17b9",
+  //       permanent: false, // false = 307 Temporary Redirect
+  //     },
+  //   ];
+  // }
 };
 
 export default nextConfig;
